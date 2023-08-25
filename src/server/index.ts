91e922cddbfeaf514a73df5325ca2ee7d673d4f5
guxiuming <guxiuming@bytedance.com>
2023-08-25 11:36:52 +0800

    fix: hot-server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,106 +1,38 @@
 import express from 'express';
 import proxy from 'express-http-proxy';
-import path from 'path';
 import webpack from 'webpack';
 import webpackDevMid from 'webpack-dev-middleware';
 import webpackHotMid from 'webpack-hot-middleware';
-import isObject from 'is-object';
-import router from './request';
+import webpackHotServerMid from 'webpack-hot-server-middleware';
+const webpackConfig = require('./config/webpack.config');
+const compiler = webpack(webpackConfig as any[]);
+const clientCompiler = compiler.compilers.find((compiler) => compiler.name === 'client');
+const clientConfig = webpackConfig.find((config: any) => config.name === 'client');
+
 const app = express();
 
-import { render } from '../client/views/app/Server';
-function normalizeAssets(assets: any) {
-  if (isObject(assets)) {
-    return Object.values(assets);
-  }
+// Step 2: Attach the dev middleware to the compiler & the server
+app.use(webpackDevMid(compiler, { publicPath: clientConfig.output.publicPath, serverSideRender: true, index: false }));
 
-  return Array.isArray(assets) ? assets : [assets];
-}
+// Step 3: Attach the hot middleware to the compiler & the server
+app.use(
+  webpackHotMid(clientCompiler as any, {
+    log: console.log,
+    path: '/__webpack_hmr',
+    heartbeat: 10 * 1000
+  })
+);
+app.use(webpackHotServerMid(compiler as any));
 
-function useHMR() {
-  // Step 1: Create & configure a webpack compiler
-  var webpackConfig = require('../../config/webpack.client');
-  var compiler = webpack(webpackConfig);
+app.use(
+  '/api',
+  proxy('http://127.0.0.1:3001', {
+    proxyReqPathResolver(req) {
+      return '/api' + req.path;
+    }
+  })
+);
 
-  // Step 2: Attach the dev middleware to the compiler & the server
-  app.use(webpackDevMid(compiler));
-
-  // Step 3: Attach the hot middleware to the compiler & the server
-  app.use(
-    webpackHotMid(compiler, {
-      log: console.log,
-      path: '/__webpack_hmr',
-      heartbeat: 10 * 1000
-    })
-  );
-
-  // The following middleware would not be invoked until the latest build is finished.
-  // app.use(/^\/(\w+\/?)*$/, (req, res) => {
-  //   console.log('webpack ssr midd', req.path);
-  //   const { devMiddleware } = res.locals.webpack;
-  //   const outputFileSystem = devMiddleware.outputFileSystem;
-  //   const jsonWebpackStats = devMiddleware.stats.toJson();
-  //   const { assetsByChunkName, outputPath } = jsonWebpackStats;
-  //   console.log('--------------');
-  //   console.log(JSON.stringify(assetsByChunkName.main));
-  //   console.log('--------------');
-
-  //   // Then use `assetsByChunkName` for server-side rendering
-  //   // For example, if you have only one main chunk:
-  //   res.send(`
-  // <html>
-  //   <head>
-  //     <title>My App</title>
-  //     <style>
-  //     ${normalizeAssets(assetsByChunkName.main)
-  //       .filter((path) => path.endsWith('.css'))
-  //       .map((p) => outputFileSystem.readFileSync(path.join(outputPath, p)))
-  //       .join('\n')}
-  //     </style>
-  //   </head>
-  //   <body>
-  //     <div id="root"></div>
-  //     ${normalizeAssets(assetsByChunkName.main)
-  //       .filter((path) => path.endsWith('.js'))
-  //       .map((path) => `<script src="http://127.0.0.1:5001/${path}"></script>`)
-  //       .join('\n')}
-  //   </body>
-  // </html>
-  //   `);
-  // });
-}
-
-// app.use(express.static('public'));
-
-// app.use(
-//   '/api',
-//   proxy('http://127.0.0.1:3001', {
-//     proxyReqPathResolver(req) {
-//       return '/api' + req.path;
-//     }
-//   })
-// );
-app.use(express.static('/public'));
-useHMR();
-
-app.use('/api', router);
-
-app.get(/^\/(?!api|public|_hmr|favicon)/, async (req, res) => {
-  console.log('get *', req.path);
-
-  const content = await render(req);
-  res.set('Content-Type', 'text/html');
-  res.send(`<html>
-  <head>
-  <title>hello</title>
-  <link rel="stylesheet" href="http://127.0.0.1:5001/public/main.css" />
-  </head>
-  <body>
-   <div id="root">${content}</div>
-  <script type="text/javascript" src="http://127.0.0.1:5001/public/index.js"></script>
-  </body>
-  </html>`);
-});
 app.listen(5001, () => {
   console.log('server start on 5001');
 });
